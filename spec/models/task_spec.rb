38f4ceb3330105c8235a3af2ca1b83cc940a2c8c
nkihbk <kenchasonakai@gmail.com>
2020-10-07 20:27:36 +0900

    new task spec

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,4 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Task, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+	it "タイトルとステータスがあれば有効なこと"
+	it "タイトルがなければ無効なこと"
+	it "ステータスがなければ無効なこと"
+	it "タイトルが重複していたら無効なこと"
 end

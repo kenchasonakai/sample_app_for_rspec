ef3bd3359734380cb5a8da537a89da7f4e4a6bd4
nkihbk <kenchasonakai@gmail.com>
2020-10-10 00:12:01 +0900

    Fix: 修正内容 user.idからuserに変更しました

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,28 +3,28 @@ require 'rails_helper'
 RSpec.describe Task, type: :model do
   it "タイトルとステータスがあれば有効なこと" do
     user = create(:user)
-    task = build(:task, user_id: user.id)
+    task = build(:task, user: user)
     expect(task).to be_valid
   end
 
   it "タイトルがなければ無効なこと" do
     user = create(:user)
-    task = build(:task, user_id: user.id, title: "")
+    task = build(:task, user: user, title: "")
     task.valid?
     expect(task.errors[:title]).to include("can't be blank")
   end
 
   it "ステータスがなければ無効なこと" do
     user = create(:user)
-    task = build(:task, user_id: user.id, status: "")
+    task = build(:task, user: user, status: "")
     task.valid?
     expect(task.errors[:status]).to include("can't be blank")
   end
 
   it "タイトルが重複していたら無効なこと" do
     user = create(:user)
-    task = create(:task, user_id: user.id)
-    task1 = build(:task, user_id: user.id)
+    task = create(:task, user: user)
+    task1 = build(:task, user: user)
     task1.valid?
     expect(task1.errors[:title]).to include("has already been taken")
   end
